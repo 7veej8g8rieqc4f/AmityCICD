423e730fdb0d742025ea9446bcd322591bf2f982
7veej8g8rieqc4f <tishagoel9@gmail.com>
2026-09-07 19:36:12 +0530

    Add third line output to hello.java

--- javaproject/hello.java
+++ javaproject/hello.java
@@ -3,5 +3,6 @@ public class hello{
     public static void main(String[] args) {
         System.out.println("Hello java");
         System.out.println("This is 2nd line");
+        System.out.println("This is my 3rd page");
     }
 }
